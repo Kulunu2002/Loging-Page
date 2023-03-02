e569edb4cd87e0a78c1cf03e40fb5ea9a22aa022
Kulunu Kasthuri <kulunukasthuri38@gmail.com>
2023-03-02 21:47:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import './loginStyle.css'
+import axios from 'axios';
+import { useState } from 'react';
+
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const url = 'http://localhost:6000/test'
+    try {
+      const submitData = {
+        email: email,
+        password: password
+      };
+      const response = await axios.post(url, submitData);
+      console.log(response);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='loginPage'>
+
+          <div className='form'>
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us please <br /> sing in with you personal info.</p>
+
+
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(handleEmail) => {
+                setEmail(handleEmail.target.value)
+              }} />
+
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(handlePassword) => {
+                setPassword(handlePassword.target.value)
+              }} />
+
+            <button type="submit">SING IN</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
-
 export default App;
